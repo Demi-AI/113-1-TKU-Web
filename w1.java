ed042f46292b83fe6714a6ca9cf6423c00f45ac7
王羽蜨 <demi921227@gmail.com>
2024-09-26 22:54:54 +0800

    add w1.java

---
+++ w1.java
@@ -0,0 +1,8 @@
+public class w1 {
+    public static void main(String[] args){
+        // Declare a boolean variable and initialize it to true to indicate that the light is on
+        boolean isLightOn = true;
+        // Output the message
+        System.out.println("The current light status is:" + isLightOn);
+    }
+}
