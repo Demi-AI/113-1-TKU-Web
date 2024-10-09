e7a2445bd7f224d3e739c97f57643737e847a09b
王羽蜨 <demi921227@gmail.com>
2024-10-09 09:49:13 +0800

    add html5 up

--- w1.java
+++
@@ -1,8 +0,0 @@
-public class w1 {
-    public static void main(String[] args){
-        // Declare a boolean variable and initialize it to true to indicate that the light is on
-        boolean isLightOn = true;
-        // Output the message
-        System.out.println("The current light status is:" + isLightOn);
-    }
-}
